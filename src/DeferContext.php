efa8b8a42269b6cf4a9d4dfd21a01d4e7d9b0bf0
memory-agape <memory.style.serv@gmail.com>
2020-03-15 22:20:36 +0900

    Add callbacks

--- src/DeferContext.php
+++ src/DeferContext.php
@@ -4,6 +4,10 @@ namespace PHPDeferrable;
 class DeferContext
 {
     protected $splStack;
+    protected $beforeCallbacks = [];
+    protected $afterCallbacks = [];
+    protected $everyBeforeCallbacks = [];
+    protected $everyAfterCallbacks = [];
 
     public function __construct(?\SplStack $splStack = null)
     {
@@ -17,18 +21,54 @@ class DeferContext
 
     public function __destruct()
     {
+        foreach ($this->beforeCallbacks as $callback) {
+            $callback($this);
+        }
         try {
-            while ($callback = $this->splStack->pop()) {
+            while ($callback = $this->pop()) {
+                foreach ($this->everyBeforeCallbacks as $everyCallback) {
+                    $everyCallback($this);
+                }
                 $callback();
+                foreach ($this->everyAfterCallbacks as $everyCallback) {
+                    $everyCallback($this);
+                }
             }
         } catch (\RuntimeException $e) {
 
+        }
+        foreach ($this->afterCallbacks as $callback) {
+            $callback($this);
         }
     }
 
     public function defer(callable $callback): self
     {
         $this->splStack->push($callback);
+        return $this;
+    }
+
+    public function everyBefore(callable $callback): self
+    {
+        $this->everyBeforeCallbacks[] = $callback;
+        return $this;
+    }
+
+    public function everyAfter(callable $callback): self
+    {
+        $this->everyAfterCallbacks[] = $callback;
+        return $this;
+    }
+
+    public function before(callable $callback): self
+    {
+        $this->beforeCallbacks[] = $callback;
+        return $this;
+    }
+
+    public function after(callable $callback): self
+    {
+        $this->afterCallbacks[] = $callback;
         return $this;
     }
 }
