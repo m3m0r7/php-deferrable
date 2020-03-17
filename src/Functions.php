69c47374b0aeaab38918de22919a3082825c8951
memory-agape <memory.style.serv@gmail.com>
2020-03-17 09:35:24 +0900

    Update

--- src/Functions.php
+++ src/Functions.php
@@ -3,6 +3,15 @@
 namespace PHPDeferrable;
 
 if (!function_exists('deferrable')) {
+    /**
+     * Allows to defer the specified function or class
+     *
+     * @param callable|string $targetClass callable or class path
+     * @param mixed ...$arguments pass parameters into class constructor
+     *
+     * @return DeferrableInterface|mixed
+     * @throws \ReflectionException
+     */
     function deferrable($targetClass, ...$arguments)
     {
         return Deferrable::makeContextManipulator(
